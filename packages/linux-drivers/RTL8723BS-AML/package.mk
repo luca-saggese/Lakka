268563b270841dceeb75c710c6f102288724a884
luca-saggese <lsaggese@LvXs-MacBook-Pro-2.local>
2017-01-03 14:39:49 +0100

    add firmwares

--- packages/linux-drivers/RTL8723BS-AML/package.mk
+++ packages/linux-drivers/RTL8723BS-AML/package.mk
@@ -51,4 +51,5 @@ make_target() {
 makeinstall_target() {
   mkdir -p $INSTALL/lib/modules/$(get_module_dir)/$PKG_NAME
     cp *.ko $INSTALL/lib/modules/$(get_module_dir)/$PKG_NAME
+  cp *.bin $INSTALL/lib/firmware/rtlwifi
 }
